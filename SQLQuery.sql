d8b66f1c891bb8383be1113bc8ee816fd6d87ffd
Imraansyedd <145869872+Imraansyedd@users.noreply.github.com>
2023-09-24 12:33:02 -0500

    Add files via upload

---
+++ SQLQuery.sql
@@ -0,0 +1,29 @@
+--CREATE DATABASE Task1;
+
+--USE task1;
+
+
+--CREATE TABLE Employeez (
+--    EmployeeID INT PRIMARY KEY,
+--    FirstName VARCHAR(50),
+--    LastName VARCHAR(50),
+--    Email VARCHAR(100),
+--    PhoneNumber VARCHAR(20),
+--    Department VARCHAR(50),
+--    Salary DECIMAL(10, 2)
+--);
+
+--INSERT INTO Employeez (EmployeeID, FirstName, LastName, Email, PhoneNumber, Department, Salary)
+--  VALUES
+--    (1, 'John', 'Doe', 'john.doe@example.com', '555-123-4567', 'HR', 50000.00),
+--    (2, 'Jane', 'Smith', 'jane.smith@example.com', '555-987-6543', 'IT', 60000.00),
+--     (3, 'Michael', 'Johnson', 'michael.johnson@example.com', '555-222-3333', 'Sales', 55000.00),
+--     (4, 'Emily', 'Brown', 'emily.brown@example.com', '555-444-5555', 'HR', 52000.00),
+--     (5, 'David', 'Wilson', 'david.wilson@example.com', '555-777-8888', 'IT', 65000.00),
+--     (6, 'Susan', 'Lee', 'susan.lee@example.com', '555-999-0000', 'Sales', 58000.00),
+--     (7, 'Robert', 'Garcia', 'robert.garcia@example.com', '555-111-2222', 'IT', 62000.00),
+--     (8, 'Karen', 'Miller', 'karen.miller@example.com', '555-333-4444', 'HR', 53000.00),
+--     (9, 'James', 'Harris', 'james.harris@example.com', '555-666-7777', 'Sales', 59000.00),
+--     (10, 'Lisa', 'Martinez', 'lisa.martinez@example.com', '555-888-9999', 'IT', 64000.00);
+
+select *from Employeez;
